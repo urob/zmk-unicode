42cd4d4f0a621f60590c4c58f2dad5f8550f3102
urob <978080+urob@users.noreply.github.com>
2025-08-07 14:49:56 -0400

    Switch to two param API

--- include/dt-bindings/zmk-unicode/uc.h
+++ include/dt-bindings/zmk-unicode/uc.h
@@ -1,16 +1,19 @@
 #pragma once
 
-//                               INPUT_START              | INPUT_STOP
-#define UC_MODE_MACOS 0       // press LALT               | release LALT
-#define UC_MODE_LINUX 1       // tap LC(LS(U))            | tap SPACE
-#define UC_MODE_LINUX_ALT 2   // press LC(LSHFT) tap U    | tap SPACE release LC(LSHFT)
-#define UC_MODE_WIN_COMPOSE 4 // tap RALT U               | tap RET
-#define UC_MODE_WIN_ALT 3     // press LALT tap KP_PLUS   | release LALT
-#define UC_MODE_EMACS 5       // tap LC(X) N8 RET         | tap RET
+#define UC_NONE 0x00
+#define UC_SELECT_INPUT_MODE 0xffffff
 
-#define UC_MAC UC_MODE_MACOS
-#define UC_LIN UC_MODE_LINUX
-#define UC_LINALT UC_MODE_LINUX_ALT
-#define UC_WINC UC_MODE_WIN_COMPOSE
-#define UC_WINALT UC_MODE_WIN_ALT
-#define UC_EMAC UC_MODE_EMACS
+//                                  INPUT_START              | INPUT_STOP
+#define UC_MODE_MACOS 0x00       // press LALT               | release LALT
+#define UC_MODE_LINUX 0x01       // tap LC(LS(U))            | tap SPACE
+#define UC_MODE_LINUX_ALT 0x02   // press LC(LSHFT) tap U    | tap SPACE release LC(LSHFT)
+#define UC_MODE_WIN_COMPOSE 0x03 // tap RALT U               | tap RET
+#define UC_MODE_WIN_ALT 0x04     // press LALT tap KP_PLUS   | release LALT
+#define UC_MODE_EMACS 0x05       // tap LC(X) N8 RET         | tap RET
+
+#define UC_SET_MACOS UC_SELECT_INPUT_MODE UC_MODE_MACOS
+#define UC_SET_LINUX UC_SELECT_INPUT_MODE UC_MODE_LINUX
+#define UC_SET_LINUX_ALT UC_SELECT_INPUT_MODE UC_MODE_LINUX_ALT
+#define UC_SET_WIN_COMPOSE UC_SELECT_INPUT_MODE UC_MODE_WIN_COMPOSE
+#define UC_SET_WIN_ALT UC_SELECT_INPUT_MODE UC_MODE_WIN_ALT
+#define UC_SET_EMACS UC_SELECT_INPUT_MODE UC_MODE_EMACS
--- include/zmk-unicode/send_string.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <zmk/behavior.h>
-
-void send_string(const struct zmk_behavior_binding_event *event, const char *codepoint);
