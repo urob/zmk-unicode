a6b48c6713a05b48971b1d91e90b2c658a280f7a
urob <978080+urob@users.noreply.github.com>
2025-09-08 16:09:38 -0400

    Port Serbian keys

---
+++ include/zmk-unicode/keys/serbian.h
@@ -0,0 +1,47 @@
+/*
+ * Serbian unicode characters.
+ *
+ * This file was automatically converted from 'serbian.dtsi',
+ * included in the zmk-helpers module.
+ */
+
+#pragma once
+
+// Cyrillic letters.
+#define UC_SR_A       0x430  0x410  // а/А
+#define UC_SR_B       0x431  0x411  // б/Б
+#define UC_SR_V       0x432  0x412  // в/В
+#define UC_SR_G       0x433  0x413  // г/Г
+#define UC_SR_D       0x434  0x414  // д/Д
+#define UC_SR_DJ      0x452  0x402  // ђ/Ђ
+#define UC_SR_E       0x435  0x415  // е/Е
+#define UC_SR_ZH      0x436  0x416  // ж/Ж
+#define UC_SR_Z       0x437  0x417  // з/З
+#define UC_SR_I       0x438  0x418  // и/И
+#define UC_SR_J       0x458  0x408  // ј/Ј
+#define UC_SR_K       0x43a  0x41a  // к/К
+#define UC_SR_L       0x43b  0x41b  // л/Л
+#define UC_SR_LJ      0x459  0x409  // љ/Љ
+#define UC_SR_M       0x43c  0x41c  // м/М
+#define UC_SR_N       0x43d  0x41d  // н/Н
+#define UC_SR_NJ      0x45a  0x40a  // њ/Њ
+#define UC_SR_O       0x43e  0x41e  // о/О
+#define UC_SR_P       0x43f  0x41f  // п/П
+#define UC_SR_R       0x440  0x420  // р/Р
+#define UC_SR_S       0x441  0x421  // с/С
+#define UC_SR_T       0x442  0x422  // т/Т
+#define UC_SR_CU      0x45b  0x40b  // ћ/Ћ
+#define UC_SR_U       0x443  0x423  // у/У
+#define UC_SR_F       0x444  0x424  // ф/Ф
+#define UC_SR_H       0x445  0x425  // х/Х
+#define UC_SR_C       0x446  0x426  // ц/Ц
+#define UC_SR_CH      0x447  0x427  // ч/Ч
+#define UC_SR_DZ      0x45f  0x40f  // џ/Џ
+#define UC_SR_SH      0x448  0x428  // ш/Ш
+
+// Latin letters.
+#define UC_SR_LAT_CH  0x10d  0x10c  // č/Č
+#define UC_SR_LAT_CU  0x107  0x106  // ć/Ć
+#define UC_SR_LAT_DJ  0x111  0x110  // đ/Đ
+#define UC_SR_LAT_SH  0x161  0x160  // š/Š
+#define UC_SR_LAT_ZH  0x17e  0x17d  // ž/Ž
