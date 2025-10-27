52142a4258393975aad183da5f118dc1cf55e1e9
urob <978080+urob@users.noreply.github.com>
2025-10-27 15:46:37 -0400

    Port Catalan keys

---
+++ include/zmk-unicode/keys/catalan.h
@@ -0,0 +1,21 @@
+/*
+ * Catalan unicode characters.
+ *
+ * This file was automatically converted from 'catalan.dtsi',
+ * included in the zmk-helpers module.
+ */
+
+#pragma once
+
+#define UC_CA_A_GRAVE      0xe0  0xc0  // à/À
+#define UC_CA_A_ACUTE      0xe1  0xc1  // á/Á
+#define UC_CA_C_CEDILLA    0xe7  0xc7  // ç/Ç
+#define UC_CA_E_GRAVE      0xe8  0xc8  // è/È
+#define UC_CA_E_ACUTE      0xe9  0xc9  // é/É
+#define UC_CA_I_ACUTE      0xed  0xcd  // í/Í
+#define UC_CA_I_DIAERESIS  0xef  0xcf  // ï/Ï
+#define UC_CA_O_GRAVE      0xf2  0xd2  // ò/Ò
+#define UC_CA_O_ACUTE      0xf3  0xd3  // ó/Ó
+#define UC_CA_U_GRAVE      0xf9  0xd9  // ù/Ù
+#define UC_CA_U_ACUTE      0xfa  0xda  // ú/Ú
+#define UC_CA_U_DIAERESIS  0xfc  0xdc  // ü/Ü
